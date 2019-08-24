4c7d03b27fe76aa9c45d6a9d4cfcd0796a4a3ec3
michaeladamstrickland <strickland1514@gmail.com>
2019-08-24 09:43:17 -0600

    new

--- config/connection.js
+++ config/connection.js
@@ -4,10 +4,10 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "Poker247",
-        database: "burgers_db"
+        host: "ffn96u87j5ogvehy.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        user: "rvv0hhz8ayllikvv",
+        password: "ub5n4eayd6axwbly",
+        database: "n3s0puq4tsrcl249"
     });
 };
 
